b1313fac018da0329978371002bbca7b4026033c
Greg Dean <dean.greg@gmail.com>
2016-09-03 16:49:57 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var handlebars = require('gulp-compile-handlebars');
+var inline = require('gulp-inline');
+var htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename');
+var gulp = require('gulp');
+
+
+exports.fbc = function(data, dest){
+    data.actors = JSON.parse(fs.readFileSync('actors.json')).results;
+    var opts = { };
+
+    helpers({
+        handlebars: handlebars.Handlebars
+    });
+
+    gulp.src('fb.hbs')
+        .pipe(handlebars(data, opts))
+        .pipe(inline())
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            minifyJS:true,
+            minifyCSS:true,
+            removeComments:true
+        }))
+        .pipe(rename('fb.html'))
+        .pipe(gulp.dest(dest));
+};
